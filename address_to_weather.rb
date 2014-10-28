d76ac8c8c5f4a2298b6de0a6e3ec23caca4bd713
nyjgary86 <garyng@wireless-111-71.uchicago.edu>
2014-10-27 21:48:33 -0500

    Done

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -15,8 +15,21 @@ url_safe_address = URI.encode(the_address)
 
 # Your code goes below.
 
-# Ultimately, we want the following line to work when uncommented:
+full_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_address
+raw_data = open(full_url).read
+parsed_data = JSON.parse(raw_data)
 
-# puts "The current temperature at #{the_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+the_latitude = parsed_data["results"][0]["geometry"]["location"]["lat"].to_s
+the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"].to_s
+
+full_url = "https://api.forecast.io/forecast/048adcb08a629d1532b3b963652b9288/" + the_latitude + "," + the_longitude
+raw_data = open(full_url).read
+parsed_data = JSON.parse(raw_data)
+
+the_temperature = parsed_data["currently"]["temperature"]
+the_hour_outlook = parsed_data["hourly"]["data"][0]["summary"]
+the_day_outlook = parsed_data["daily"]["data"][0]["summary"]
+
+puts "The current temperature at #{the_address} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
--- coords_to_weather.rb
+++ coords_to_weather.rb
@@ -19,8 +19,16 @@ the_longitude = gets.chomp
 #   address_to_coords.rb to read from a remote API and parse
 #   the results.
 
+full_url = "https://api.forecast.io/forecast/048adcb08a629d1532b3b963652b9288/" + the_latitude + "," + the_longitude
+raw_data = open(full_url).read
+parsed_data = JSON.parse(raw_data)
+
+the_temperature = parsed_data["currently"]["temperature"]
+the_hour_outlook = parsed_data["hourly"]["data"][0]["summary"]
+the_day_outlook = parsed_data["daily"]["data"][0]["summary"]
+
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
